97a36ca1f8782dcb815814e6f746f4440455e02d
Samod Achintha <samodachintha@gmail.com>
2026-02-01 08:27:44 +0530

    Addded P.Jason

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+console.log('Hello, World!');
+const add = (a, b) => a + b;
+console.log(`2 + 3 = ${add(2, 3)}`);
+export { add };
+export default function greet(name) {
+    return `Hello, ${name}!`;
+}
 
